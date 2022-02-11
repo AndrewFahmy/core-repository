496a1f0fda97bf91e26c9e8da3801292a53810db
Andrew Fahmy <andrew.fahmy@outlook.com>
2022-02-11 15:26:50 +0200

    adding support for different injection lifetimes

--- FrameworkRepository/RegisterRepositories.cs
+++ FrameworkRepository/RegisterRepositories.cs
@@ -5,22 +5,33 @@ namespace FrameworkRepository
 {
     public static class RegisterRepositories
     {
-        public static void AddRepositories(this IServiceCollection services, bool isConsole = false)
+        public static void AddRepositories(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
         {
-            if (isConsole)
+            switch (lifetime)
             {
-                services.AddSingleton(typeof(IRepository<,>), typeof(Repository<,>));
-                services.AddSingleton(typeof(IRawRepository<>), typeof(RawRepository<>));
-                services.AddSingleton(typeof(IReadOnlyRepository<,>), typeof(ReadOnlyRepository<,>));
-                services.AddSingleton(typeof(ITransactionRepository<,>), typeof(TransactionRepository<,>));
+                case ServiceLifetime.Singleton:
+                    services.AddSingleton(typeof(IRepository<,>), typeof(Repository<,>));
+                    services.AddSingleton(typeof(IRawRepository<>), typeof(RawRepository<>));
+                    services.AddSingleton(typeof(IReadOnlyRepository<,>), typeof(ReadOnlyRepository<,>));
+                    services.AddSingleton(typeof(ITransactionRepository<,>), typeof(TransactionRepository<,>));
+                    break;
+
+                case ServiceLifetime.Transient:
+                    services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+                    services.AddTransient(typeof(IRawRepository<>), typeof(RawRepository<>));
+                    services.AddTransient(typeof(IReadOnlyRepository<,>), typeof(ReadOnlyRepository<,>));
+                    services.AddTransient(typeof(ITransactionRepository<,>), typeof(TransactionRepository<,>));
+                    break;
+
+
+                default:
+                    services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+                    services.AddScoped(typeof(IRawRepository<>), typeof(RawRepository<>));
+                    services.AddScoped(typeof(IReadOnlyRepository<,>), typeof(ReadOnlyRepository<,>));
+                    services.AddScoped(typeof(ITransactionRepository<,>), typeof(TransactionRepository<,>));
+                    break;
             }
-            else
-            {
-                services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
-                services.AddScoped(typeof(IRawRepository<>), typeof(RawRepository<>));
-                services.AddScoped(typeof(IReadOnlyRepository<,>), typeof(ReadOnlyRepository<,>));
-                services.AddScoped(typeof(ITransactionRepository<,>), typeof(TransactionRepository<,>));
-            }
+
         }
     }
 }
